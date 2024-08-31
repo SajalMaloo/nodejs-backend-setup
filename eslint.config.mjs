bd1d58e50418a58fe0db195d2c7193bc730ae41d
Sajal Maloo <sajal5maloo@gmail.com>
2024-09-01 01:08:46 +0530

    feat: Prettier setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config({
   languageOptions: {
@@ -14,11 +15,12 @@ export default tseslint.config({
   extends: [
     eslint.configs.recommended,
     ...tseslint.configs.recommended,
-  ],
+    eslintConfigPrettier
+],
   rules: {
     'no-console': 'error',
     quotes: ['error', 'single', {
-      allowTemplateLiterals: true,
+        allowTemplateLiterals: true,
     }]
   }
 });
